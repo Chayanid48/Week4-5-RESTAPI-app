6055554dc01e4a1999fc39894c22a7d5a00c03d1
my-react-spaapp <chayanid.sin@dome.tu.ac.th>
2025-02-25 12:34:21 +0700

    commit

--- Week4-5RestAPI-app/src/App.tsx
+++ Week4-5RestAPI-app/src/App.tsx
@@ -3,16 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ProductList from "./ProductList";
-
+import AddProduct from "./AddProduct";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [refresh, setRefresh] = useState(false);
+  const apiUrl = "http://localhost:5174";
 
   return (
     <>
      <div>
       <h1>ร้านค้าสินค้าออนไลน์</h1>
-      <ProductList apiUrl="http://localhost:5000" />
+      <AddProduct apiUrl={apiUrl} onProductAdded={() => setRefresh(!refresh)} />
+      <ProductList apiUrl={apiUrl} key={refresh} />
     </div>
 
       <div>
